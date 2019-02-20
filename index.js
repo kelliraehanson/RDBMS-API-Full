d3177d294722e8516ee2cb2b56fb9fdb93a8b309
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-02-20 15:25:33 -0600

    getting an empty array at last

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
 
-const db = knex(knexConfig);
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+// GET
+server.get('/api/cohorts', (req, res) => {
+	db.from('cohorts')
+		.then(list => {
+			res.status(200).json(list);
+		})
+		.catch(err => {
+			res.status(500).json(err);
+		});
+});
 
 const port = process.env.PORT || 4000;
 server.listen(port, () => console.log(`\nrunning on ${port}\n`));
